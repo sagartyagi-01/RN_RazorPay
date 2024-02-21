e928223efeb1b80ccd761dd23d0cc4ae29a07530
Sagar <v_sagar@jsw.in>
2024-02-21 18:12:57 +0530

    Added constants file

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const sampleImg = 'https://ih1.redbubble.net/image.1194811665.5398/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u2.jpg'
+export const amount = 100;
+export const currency = "INR";
+export const RazorPay_KEY_ID = 'rzp_test_BY7RGNeANgiJOy';
+
+export const options = {
+    description: 'Test Product',
+    image: 'https://i.imgur.com/3g7nmJC.png',
+    currency: currency,
+    key: RazorPay_KEY_ID,
+    amount: amount * 100,
+    name: 'Sagar Tyagi', 
+    theme: { color: '#F37254' }
+  }
